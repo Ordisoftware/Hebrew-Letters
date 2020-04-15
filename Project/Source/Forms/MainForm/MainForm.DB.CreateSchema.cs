dfea1110c6cb9bc68d8c8cf335b5836f8b4af170
Olivier Rogier <github.com@ordisoftware.org>
2020-04-16 00:20:20 +0200

    Update file header date

--- Project/Source/Forms/MainForm/MainForm.DB.CreateSchema.cs
+++ Project/Source/Forms/MainForm/MainForm.DB.CreateSchema.cs
@@ -11,7 +11,7 @@
 /// You may add additional accurate notices of copyright ownership.
 /// </license>
 /// <created> 2019-01 </created>
-/// <edited> 2019-09 </edited>
+/// <edited> 2020-04 </edited>
 using System;
 using System.Data.Odbc;
 using System.Windows.Forms;
