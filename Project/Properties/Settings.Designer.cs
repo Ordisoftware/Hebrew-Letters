8d0ed1493e265af8655798cb7580185114c13cc7
Olivier Rogier <github.com@ordisoftware.org>
2020-04-27 20:42:53 +0200

    Improve UI

--- Project/Properties/Settings.Designer.cs
+++ Project/Properties/Settings.Designer.cs
@@ -227,7 +227,7 @@ namespace Ordisoftware.HebrewLetters.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("392, 466")]
+        [global::System.Configuration.DefaultSettingValueAttribute("492, 566")]
         public global::System.Drawing.Size GrammarGuideFormSize {
             get {
                 return ((global::System.Drawing.Size)(this["GrammarGuideFormSize"]));
@@ -263,7 +263,7 @@ namespace Ordisoftware.HebrewLetters.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("392, 466")]
+        [global::System.Configuration.DefaultSettingValueAttribute("492, 566")]
         public global::System.Drawing.Size MethodNoticeFormSize {
             get {
                 return ((global::System.Drawing.Size)(this["MethodNoticeFormSize"]));
