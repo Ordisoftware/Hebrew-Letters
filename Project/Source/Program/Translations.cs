2f9818bfaea912db5e346a5a64e4abe7a63f3ae8
Olivier Rogier <github.com@ordisoftware.org>
2020-09-03 22:55:19 +0200

    Refactor

--- Project/Source/Program/Translations.cs
+++ Project/Source/Program/Translations.cs
@@ -24,15 +24,15 @@ namespace Ordisoftware.HebrewLetters
   static public partial class Translations
   {
 
-    static public readonly NullSafeOfStringDictionary<Language> ApplicationDescription
-      = new NullSafeOfStringDictionary<Language>
+    static public readonly TranslationsDictionary ApplicationDescription
+      = new TranslationsDictionary
       {
         { Languages.EN, "Lettriq letter-by-letter study and analysis of Hebrew words" },
         { Languages.FR, "Étude et analyse lettrique lettre-par-lettre des mots hébreux" }
       };
 
-    static public readonly NullSafeOfStringDictionary<Language> AskToRestoreLettersDefaults
-      = new NullSafeOfStringDictionary<Language>
+    static public readonly TranslationsDictionary AskToRestoreLettersDefaults
+      = new TranslationsDictionary
       {
         { Languages.EN, "Letters will be restored to their default values." + Globals.NL2 +
                         "All additions will be lost." + Globals.NL2 +
