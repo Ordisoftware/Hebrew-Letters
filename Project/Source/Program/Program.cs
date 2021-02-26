b30eb8bbd2298751612dc73b310e7af8d3ed388c
Olivier Rogier <github.com@ordisoftware.org>
2021-02-26 22:33:58 +0100

    Refactor

--- Project/Source/Program/Program.cs
+++ Project/Source/Program/Program.cs
@@ -50,6 +50,7 @@ namespace Ordisoftware.Hebrew.Letters
       Globals.SettingsUpgraded = upgrade;
       Settings.CheckUpgradeRequired(ref upgrade);
       Settings.UpgradeRequired = upgrade;
+      Globals.SettingsUpgraded = Globals.SettingsUpgraded && !Settings.FirstLaunch;
       CheckSettingsReset();
       Globals.Settings = Settings;
       Globals.MainForm = MainForm.Instance;
