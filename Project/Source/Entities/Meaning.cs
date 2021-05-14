6075e3d4aa756679373b4c5f33a200fa591a8dcf
Olivier Rogier <github.com@ordisoftware.org>
2021-05-14 10:19:51 +0200

    Add dependency

---
+++ Project/Source/Entities/Meaning.cs
@@ -0,0 +1,31 @@
+﻿/// <license>
+/// This file is part of Ordisoftware Hebrew Letters.
+/// Copyright 2016-2021 Olivier Rogier.
+/// See www.ordisoftware.com for more information.
+/// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+/// If a copy of the MPL was not distributed with this file, You can obtain one at 
+/// https://mozilla.org/MPL/2.0/.
+/// If it is not possible or desirable to put the notice in a particular file, 
+/// then You may include the notice in a location(such as a LICENSE file in a 
+/// relevant directory) where a recipient would be likely to look for such a notice.
+/// You may add additional accurate notices of copyright ownership.
+/// </license>
+/// <created> 2021-05 </created>
+/// <edited> 2021-05 </edited>
+using System;
+using SQLite;
+
+namespace Ordisoftware.Hebrew.Letters
+{
+
+  [Table("Meanings")]
+  public class Meaning
+  {
+    [PrimaryKey]
+    public string ID { get; set; }
+    public string LetterCode { get; set; }
+    [Column("Meaning")]
+    public string Text { get; set; }
+  }
+
+}
