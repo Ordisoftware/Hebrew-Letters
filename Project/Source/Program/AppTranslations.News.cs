7cdba84dff5908dbce3c27e21ad284949f77029b
Olivier Rogier <github.com@ordisoftware.org>
2021-05-16 13:27:00 +0200

    Add text

--- Project/Source/Program/AppTranslations.News.cs
+++ Project/Source/Program/AppTranslations.News.cs
@@ -28,41 +28,54 @@ namespace Ordisoftware.Hebrew.Letters
       = new NullSafeDictionary<string, TranslationsDictionary>
       {
 
-        ["1.x"] = new TranslationsDictionary
+        ["6.0"] = new TranslationsDictionary
         {
-          [Language.EN] = "Initial release with basic combo boxes.",
+          [Language.EN] = "SQLite ODBC Driver is no more needed." + Globals.NL +
+                          "Overall performances are Optimized." + Globals.NL +
+                          "Some UI improvements and some fixes.",
 
-          [Language.FR] = "Version initiale avec boites combos de base."
+          [Language.FR] = "SQLite ODBC Driver is no more needed." + Globals.NL +
+                          "Overall performances are Optimized." + Globals.NL +
+                          "Some UI improvements and some fixes."
         },
 
-        ["2.x"] = new TranslationsDictionary
+        ["5.x"] = new TranslationsDictionary
         {
-          [Language.EN] = "Added copy list of meanings for current word." + Globals.NL +
-                          "Improved meanings." + Globals.NL +
-                          "Improved interface." + Globals.NL +
-                          "Improved preferences.",
+          [Language.EN] = "Added command-line options (see help FAQ)" + Globals.NL +
+                          "Startup speed and analysis combo boxes creation are optimized." + Globals.NL +
+                          "Data edition is only possible by one instance at a time." + Globals.NL +
+                          "Data edition is improved to have save and cancel behavior." + Globals.NL +
+                          "Improved and fixed copy, cut and paste for both Hebrew font chars and unicode." + Globals.NL +
+                          "Analysis labels are clickable to switch to letter's details." + Globals.NL +
+                          "Added New in version in the Information menu." + Globals.NL +
+                          "Added link to online Shorashim roots database." + Globals.NL +
+                          "New top menu icon to open a new window." + Globals.NL +
+                          "New context menu on letters' buttons." + Globals.NL +
+                          "New screenshot button." + Globals.NL +
+                          "New usage statistics window." + Globals.NL +
+                          "Rework of the preferences window design with more options." + Globals.NL +
+                          "Some improvements in appearance and function." + Globals.NL +
+                          "Incorporate common code written since more than one year for Hebrew Calendar." + Globals.NL +
+                          "Modification and addition of web links and online providers for the study of words and verses of the Bible." + Globals.NL +
+                          "Web links updated.",
 
-          [Language.FR] = "Ajout de la copie de la liste des significations du mot actuel." + Globals.NL +
-                          "Significations améliorées." + Globals.NL +
-                          "Interface améliorée." + Globals.NL +
-                          "Préférences améliorées."
-        },
-
-        ["3.x"] = new TranslationsDictionary
-        {
-          [Language.EN] = "Added search term button." + Globals.NL +
-                          "Added copy to and paste from unicode chars buttons." + Globals.NL +
-                          "Added online search button for several providers like Pealim and Wiktionary." + Globals.NL +
-                          "Added some preferences options." + Globals.NL +
-                          "Use advanced TextBox to allow copy, cut, paste, select all and delete." + Globals.NL +
-                          "General immprovements and some fixes.",
-
-          [Language.FR] = "Ajout d'un bouton de recherche de terme." + Globals.NL +
-                          "Ajout de boutons pour copier et coller à partir de caractères Unicode." + Globals.NL +
-                          "Ajout d'un bouton de recherche en ligne pour plusieurs fournisseurs comme Pealim et Wiktionary." + Globals.NL +
-                          "Ajout de quelques options de préférences." + Globals.NL +
-                          "Utilisation d'un TextBox avancé pour permettre de copier, couper, coller, tout sélectionner et supprimer." + Globals.NL +
-                          "Améliorations générales et quelques corrections."
+          [Language.FR] = "Ajout d'options de ligne de commande (voir la FAQ de l'aide)." + Globals.NL +
+                          "Le temps de démarrage et la création des boites combos d'analyse sont optimisé." + Globals.NL +
+                          "L'édition des données est possible seulement par une seule instance à la fois." + Globals.NL +
+                          "L'édition des données est améliorée pour avoir le comportement sauver et annuler." + Globals.NL +
+                          "Amélioration et correction du copier, couper et coller pour les caractères de police hébreu et unicode." + Globals.NL +
+                          "Les labels d'analyse sont cliquables pour basculer vers les détails de la lettre." + Globals.NL +
+                          "Ajout des Nouveautés de version dans le menu Information." + Globals.NL +
+                          "Ajout d'un lien vers une base de données en ligne de racines Shorashim." + Globals.NL +
+                          "Nouvelle icône de menu principal pour ouvrir une nouvelle fenêtre." + Globals.NL +
+                          "Nouveau menu contextuel pour les boutons des lettres." + Globals.NL +
+                          "Nouveau bouton de capture d'écran." + Globals.NL +
+                          "Nouvelle fenêtre de statistiques d'utilisation." + Globals.NL +
+                          "Refonte de la fenêtre des préférences avec ajout d'options." + Globals.NL +
+                          "Quelques améliorations d'aspect et de fonctionnement." + Globals.NL +
+                          "Incorporation du code commun écrit depuis plus d'un an pour Hebrew Calendar." + Globals.NL +
+                          "Modification et ajouts de liens web et de fournisseurs en ligne pour l'étude des mots et des versets de la Bible." + Globals.NL +
+                          "Mise à jour des liens web."
         },
 
         ["4.x"] = new TranslationsDictionary
@@ -82,64 +95,41 @@ namespace Ordisoftware.Hebrew.Letters
                           "Améliorations générales et quelques corrections."
         },
 
-        ["5.0"] = new TranslationsDictionary
+        ["3.x"] = new TranslationsDictionary
         {
-          [Language.EN] = "Added command-line options (see help FAQ)" + Globals.NL +
-                          "Startup speed and analysis combo boxes creation are optimized." + Globals.NL +
-                          "Data edition is only possible by one instance at a time." + Globals.NL +
-                          "Data edition is improved to have save and cancel behavior." + Globals.NL +
-                          "Improved and fixed copy, cut and paste for both Hebrew font chars and unicode." + Globals.NL +
-                          "Added New in version in the Information menu." + Globals.NL +
-                          "Added link to online Shorashim roots database." + Globals.NL +
-                          "New top menu icon to open a new window." + Globals.NL +
-                          "New context menu on letters' buttons." + Globals.NL +
-                          "New screenshot button." + Globals.NL +
-                          "New usage statistics window." + Globals.NL +
-                          "Rework of the preferences window design with more options." + Globals.NL +
-                          "Some improvements in appearance and function." + Globals.NL +
-                          "Incorporate common code written since more than one year for Hebrew Calendar." + Globals.NL +
-                          "Modification and addition of web links and online providers for the study of words and verses of the Bible.",
+          [Language.EN] = "Added search term button." + Globals.NL +
+                          "Added copy to and paste from unicode chars buttons." + Globals.NL +
+                          "Added online search button for several providers like Pealim and Wiktionary." + Globals.NL +
+                          "Added some preferences options." + Globals.NL +
+                          "Use advanced TextBox to allow copy, cut, paste, select all and delete." + Globals.NL +
+                          "General immprovements and some fixes.",
 
-          [Language.FR] = "Ajout d'options de ligne de commande (voir la FAQ de l'aide)." + Globals.NL +
-                          "Le temps de démarrage et la création des boites combos d'analyse sont optimisé." + Globals.NL +
-                          "L'édition des données est possible seulement par une seule instance à la fois." + Globals.NL +
-                          "L'édition des données est améliorée pour avoir le comportement sauver et annuler." + Globals.NL +
-                          "Amélioration et correction du copier, couper et coller pour les caractères de police hébreu et unicode." + Globals.NL +
-                          "Ajout des Nouveautés de version dans le menu Information." + Globals.NL +
-                          "Ajout d'un lien vers une base de données en ligne de racines Shorashim." + Globals.NL +
-                          "Nouvelle icône de menu principal pour ouvrir une nouvelle fenêtre." + Globals.NL +
-                          "Nouveau menu contextuel pour les boutons des lettres." + Globals.NL +
-                          "Nouveau bouton de capture d'écran." + Globals.NL +
-                          "Nouvelle fenêtre de statistiques d'utilisation." + Globals.NL +
-                          "Refonte de la fenêtre des préférences avec ajout d'options." + Globals.NL +
-                          "Quelques améliorations d'aspect et de fonctionnement." + Globals.NL +
-                          "Incorporation du code commun écrit depuis plus d'un an pour Hebrew Calendar." + Globals.NL +
-                          "Modification et ajouts de liens web et de fournisseurs en ligne pour l'étude des mots et des versets de la Bible."
+          [Language.FR] = "Ajout d'un bouton de recherche de terme." + Globals.NL +
+                          "Ajout de boutons pour copier et coller à partir de caractères Unicode." + Globals.NL +
+                          "Ajout d'un bouton de recherche en ligne pour plusieurs fournisseurs comme Pealim et Wiktionary." + Globals.NL +
+                          "Ajout de quelques options de préférences." + Globals.NL +
+                          "Utilisation d'un TextBox avancé pour permettre de copier, couper, coller, tout sélectionner et supprimer." + Globals.NL +
+                          "Améliorations générales et quelques corrections."
         },
 
-        ["5.1"] = new TranslationsDictionary
+        ["2.x"] = new TranslationsDictionary
         {
-          [Language.EN] = "Analysis labels are clickable to switch to letter's details." + Globals.NL +
-                          "Web links updated.",
+          [Language.EN] = "Added copy list of meanings for current word." + Globals.NL +
+                          "Improved meanings." + Globals.NL +
+                          "Improved interface." + Globals.NL +
+                          "Improved preferences.",
 
-          [Language.FR] = "Les labels d'analyse sont cliquables pour basculer vers les détails de la lettre." + Globals.NL +
-                          "Mise à jour des liens web."
+          [Language.FR] = "Ajout de la copie de la liste des significations du mot actuel." + Globals.NL +
+                          "Significations améliorées." + Globals.NL +
+                          "Interface améliorée." + Globals.NL +
+                          "Préférences améliorées."
         },
 
-        ["5.2"] = new TranslationsDictionary
+        ["1.x"] = new TranslationsDictionary
         {
-          [Language.EN] = "Fix exception in analysis meanings comboboxes introduced in previous version." + Globals.NL +
-                          "Web links updated.",
+          [Language.EN] = "Initial release with basic combo boxes.",
 
-          [Language.FR] = "Correction d'une exception dans les listes déroulantes des significations d'analyse introduite dans la version précédente.." + Globals.NL +
-                          "Mise à jour des liens web."
-        },
-
-        ["5.3"] = new TranslationsDictionary
-        {
-          [Language.EN] = "Maintenance release.",
-
-          [Language.FR] = "Version de maintenance."
+          [Language.FR] = "Version initiale avec boites combos de base."
         },
 
       };
