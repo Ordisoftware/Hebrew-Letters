f7317a97fa56d808baf5ba7729f0663d02a6228e
Olivier Rogier <github.com@ordisoftware.org>
2022-03-24 20:21:52 +0100

    Refactor

--- Project/Source/Forms/Config/PreferencesForm.cs
+++ Project/Source/Forms/Config/PreferencesForm.cs
@@ -11,7 +11,7 @@
 /// You may add additional accurate notices of copyright ownership.
 /// </license>
 /// <created> 2019-09 </created>
-/// <edited> 2022-02 </edited>
+/// <edited> 2022-03 </edited>
 namespace Ordisoftware.Hebrew.Letters;
 
 using KVPImageExportTarget = KeyValuePair<ImageExportTarget, string>;
--- Project/Source/Forms/MainForm/MainForm.Initialize.cs
+++ Project/Source/Forms/MainForm/MainForm.Initialize.cs
@@ -11,7 +11,7 @@
 /// You may add additional accurate notices of copyright ownership.
 /// </license>
 /// <created> 2019-01 </created>
-/// <edited> 2021-11 </edited>
+/// <edited> 2022-03 </edited>
 namespace Ordisoftware.Hebrew.Letters;
 
 using Microsoft.Win32;
