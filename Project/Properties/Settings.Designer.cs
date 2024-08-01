8863fb40ea49e8611b1ccfc9a54d3df5ad4a57e3
Olivier Rogier <github.com@ordisoftware.org>
2024-08-01 10:44:23 +0200

    Add gematria for final letters

--- Project/Properties/Settings.Designer.cs
+++ Project/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Ordisoftware.Hebrew.Letters.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -849,6 +849,18 @@ namespace Ordisoftware.Hebrew.Letters.Properties {
             }
             set {
                 this["DefaultConcordanceURL"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool ShowFinalValues {
+            get {
+                return ((bool)(this["ShowFinalValues"]));
+            }
+            set {
+                this["ShowFinalValues"] = value;
             }
         }
     }
